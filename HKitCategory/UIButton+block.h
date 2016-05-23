26d49d0d8ac414e557136efeeace5193d1de9aec
hare27 <hare27@foxmail.com>
2016-05-23 22:19:50 +0800

    1.0 添加了一个button的分类
    创建button的时候，可以通过block的方式给button添加点击事件

---
+++ HKitCategory/UIButton+block.h
@@ -0,0 +1,17 @@
+//
+//  UIButton+block.h
+//  runTimeDemo
+//
+//  Created by hare27 on 16/5/23.
+//  Copyright © 2016年 hare27. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+typedef void(^ControlEventBlock)(void);
+
+@interface UIButton (block)
+
++(instancetype)h_buttonWithType:(UIButtonType)type addControlEvent:(UIControlEvents)controlEvent useBlock:(ControlEventBlock)block;
+
+@end
---
+++ HKitCategory/UIButton+block.m
@@ -0,0 +1,48 @@
+//
+//  UIButton+block.m
+//  runTimeDemo
+//
+//  Created by hare27 on 16/5/23.
+//  Copyright © 2016年 hare27. All rights reserved.
+//
+
+#import "UIButton+block.h"
+#import <objc/runtime.h>
+
+//static const char *associatedKey = @"associatedKey";
+
+#define kControlEventString @"controlEvent"
+
+@interface UIButton()
+
+@property(nonatomic,copy)ControlEventBlock controlEventBlock;
+
+@end
+
+@implementation UIButton (block)
+
+#pragma mark - 生命周期 Life Circle
++(instancetype)h_buttonWithType:(UIButtonType)type addControlEvent:(UIControlEvents)controlEvent useBlock:(void(^)(void))block{
+    UIButton *button = [self buttonWithType:type];
+    button.controlEventBlock = block;
+    [button addTarget:button action:@selector(useBlock) forControlEvents:controlEvent];
+    return button;
+}
+
+-(void)useBlock{
+    if (self.controlEventBlock) {
+        self.controlEventBlock();
+    }
+}
+
+#pragma mark - 方法 Methods
+
+#pragma mark - getter and setter
+-(ControlEventBlock)controlEventBlock{
+    return objc_getAssociatedObject(self, kControlEventString);
+}
+-(void)setControlEventBlock:(ControlEventBlock)controlEventBlock{
+    objc_setAssociatedObject(self, kControlEventString, controlEventBlock, OBJC_ASSOCIATION_COPY_NONATOMIC);
+}
+
+@end
